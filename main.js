b6b6b4cd6aba0e8eb309a3d45a075b7a0940f9c9
wojtekpirog <114958949+wojtekpirog@users.noreply.github.com>
2024-10-22 19:38:32 +0200

    Create an accordion

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
-let stepIndicator;
-let formPages;
-let progressBar;
-let progressSteps;
-let prevButton;
-let nextButton;
-
-// Bieżący krok formularza:
-let currentStep = 1;
+let accordion;
+let accordionBtns;
+let accordionTextBoxes;
 
 function main() {
   getElements();
@@ -14,69 +8,36 @@ function main() {
 };
 
 function getElements() {
-  stepIndicator = document.querySelector(".app__form-indicator");
-  formPages = document.querySelectorAll(".app__form-page");
-  progressBar = document.querySelector(".app__progress-bar");
-  progressSteps = document.querySelectorAll(".app__progress-step");
-  prevButton = document.querySelector(".app__button--prev");
-  nextButton = document.querySelector(".app__button--next");
+  accordion = document.querySelector(".app__accordion");
+  accordionBtns = accordion.querySelectorAll(".app__accordion-button");
+  accordionTextBoxes = accordion.querySelectorAll(".app__accordion-text-box");
 };
 
 function addListeners() {
-  prevButton.addEventListener("click", goToPrevStep);
-  nextButton.addEventListener("click", goToNextStep);
-};
-
-const goToNextStep = () => {
-  currentStep += 1;
-  // Czy osiągnięto ostatni krok formularza?
-  currentStep > progressSteps.length && (currentStep = progressSteps.length);
-  handleProgress();
+  accordionBtns.forEach((accordionBtn) => accordionBtn.addEventListener("click", handleAccordionBoxes));
+  window.addEventListener("click", closeOutsideAccordion);
 }
 
-const goToPrevStep = () => {
-  currentStep -= 1;
-  // Czy osiągnięto pierwszy krok formularza?
-  currentStep < 1 && (currentStep = 1);
-  handleProgress();
-}
-
-const handleProgress = () => {
-  progressSteps.forEach((step, index) => {
-    step.removeAttribute("aria-current");
-    step.classList.remove("app__progress-step--active");
-
-    currentStep === index + 1 && step.setAttribute("aria-current", "step");
-    currentStep >= index + 1 && step.classList.add("app__progress-step--active");
-  });
-  
-  progressBar.style.setProperty("--width", `${100 * ((currentStep - 1) / (progressSteps.length - 1))}%`);
-  stepIndicator.textContent = `You are on step ${currentStep} of ${progressSteps.length}.`;
-  changeFormPage();
-  handleDisabled();
-}
-
-const handleDisabled = () => {
-  if (currentStep === progressSteps.length) {
-    nextButton.disabled = true;
-  } else if (currentStep === 1) {
-    prevButton.disabled = true;
-  } else {
-    prevButton.disabled = false;
-    nextButton.disabled = false;
+function handleAccordionBoxes() {
+  if (this.nextElementSibling.classList.contains("app__accordion-text-box--active")) {
+    this.nextElementSibling.classList.remove("app__accordion-text-box--active");
+    return ;
   }
+
+  closeAccordionBoxes();
+  this.nextElementSibling.classList.toggle("app__accordion-text-box--active");
 }
 
-const changeFormPage = () => {
-  formPages.forEach((page, index) => {
-    if (currentStep === index + 1) {
-      page.classList.add("app__form-page--active");
-      page.setAttribute("aria-current", "page");
-    } else {
-      page.classList.remove("app__form-page--active");
-      page.removeAttribute("aria-current");
-    };
-  });
+const closeAccordionBoxes = () => {
+  accordion.querySelectorAll(".app__accordion-text-box--active").forEach((textBox) => textBox.classList.remove("app__accordion-text-box--active"));
+}
+
+const closeOutsideAccordion = (event) => {
+  event.target.classList.contains("app__accordion-button") || 
+  event.target.classList.contains("app__accordion-text") ||
+  event.target.classList.contains("app__accordion-text-box")
+    ? false
+    : closeAccordionBoxes();
 }
 
 document.addEventListener("DOMContentLoaded", main);
